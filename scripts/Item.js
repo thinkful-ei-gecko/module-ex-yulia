1d3f96b906e52b632db81a28b285da500cfde53f
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-08-21 08:51:36 -0700

    updated Item

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,4 +1,20 @@
+'use strict';
+/* global cuid*/
 const Item = (function() {
-    const foo = 'bar';
-    return {};
-}());
+  const validateName= function(name) {
+    if (!name) {
+      throw new Error('Name does not exist.');
+    }
+  };
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  };
+  return {
+    validateName,
+    create
+  };
+} ());
--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,17 @@
 function main() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  shoppingList.render();
 }
 console.log(Item);
-console.log(foo);
 
 $(main);
